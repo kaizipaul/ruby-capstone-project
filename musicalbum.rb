4da9aae9e42b7f109a130c835f69a37d492aabdb
Jonah Wambua <djmrjay@gmail.com>
2023-01-24 14:15:00 +0300

    Add unit tests

--- musicalbum.rb
+++ musicalbum.rb
@@ -19,39 +19,3 @@ class MusicAlbum < Item
     super || @on_spotify ? true : false
   end
 end
-
-# Methods
-def list_music
-  if @music_list.empty?
-    puts 'Music list is empty'
-  else
-    @music_list.each_with_index do |music, index|
-      puts "(#{index}) ID: #{music.id} On Spotify: \"#{music.on_spotify}\", Publish Date: #{music.publish_date} Archived: #{music.archived}" # rubocop:disable Layout/LineLength
-    end
-  end
-end
-
-def create_music
-  puts 'Is it on spotify? [y/n]: '
-  on_spotify = gets.chomp
-  puts 'Please state when was the music published [yyyy/mm/dd]:'
-  publish_date = gets.chomp
-  print 'Music Album Added Succesfully'
-  new_music = MusicAlbum.new(on_spotify, publish_date)
-  @music_list.push(new_music)
-end
-
-def add_music(on_spotify, publish_date)
-  new_music = MusicAlbum.new(on_spotify, publish_date)
-  @music_list << new_music
-end
-
-def list_labels
-  if @label_list.empty?
-    puts 'Label list is empty'
-  else
-    @label_list.each_with_index do |label, index|
-      puts "(#{index}) Title: \"#{label.title}\", Color: #{label.color}"
-    end
-  end
-end

fa5dcd2633c32ff2dcf6238ea01553a23e84a7ad
Oluwatoyin Olaoye <mindprepopps@gmail.com>
2023-01-24 13:12:21 +0100

    Add Game class and inherit prop from class of Item

---
+++ game.rb
@@ -0,0 +1,27 @@
+require 'date'
+require_relative './item'
+
+
+class Game < Item
+  attr_accessor :multiplayer, :last_played_at, :publish_date
+  attr_reader :id
+
+  def initialize(multiplayer, last_played_at, publish_date)
+    super(id = Random.rand(1..1000))
+    @id = id
+    @multiplayer = multiplayer
+    @last_played_at = last_played_at
+    @publish_date = publish_date
+    @archived = can_be_archived?
+  end
+
+  private
+
+  def can_be_archived?
+    @archived = if super || Date.today - Date.parse(@last_played_at) > 365 * 2
+                  true
+                else
+                  false
+                end
+  end
+end

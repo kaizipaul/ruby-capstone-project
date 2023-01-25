409703c4390366ac277d7d48ee0f590697bb7175
kaizipaul <kaizipaul@gmail.com>
2023-01-25 22:20:14 +0300

    Add code to read JSON files

--- main.rb
+++ main.rb
@@ -1,11 +1,13 @@
 require_relative './app'
 require_relative './menu'
 require_relative './game_file'
+require_relative './book_data'
 
 def main
   app = App.new
   Dir.exist?('./storage') ? app.read_files : nil
   Dir.exist?('./storage') ? app.read_files2 : nil
+  Dir.exist?('./storage') ? app.read_booklabel : nil
   loop do
     menu(app)
   end

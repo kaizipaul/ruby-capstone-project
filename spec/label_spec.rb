e25317cc5082963a94dbc35147df4a074af761ec
kaizipaul <kaizipaul@gmail.com>
2023-01-25 22:30:23 +0300

    Add test code for Label Class

---
+++ spec/label_spec.rb
@@ -0,0 +1,33 @@
+require_relative '../label'
+
+describe 'Unit testing for Label Class' do
+  before :each do
+    @label = Label.new('Gift', 'Red')
+  end
+
+  context 'initialize object' do
+    it 'should be an instance of Label' do
+      expect(@label).to be_instance_of Label
+    end
+
+    it 'should not be an instance of Item' do
+      expect(@label).not_to be_instance_of Item
+    end
+
+    it 'should be a kind of Item' do
+      expect(@label).to be_kind_of Item
+    end
+  end
+
+  context 'Label title' do
+    it 'should be "Gift"' do
+      expect(@author.title).to eql 'Gift'
+    end
+  end
+
+  context 'Label Color' do
+    it 'should be "Red"' do
+      expect(@author.color).to eql 'Red'
+    end
+  end
+end
